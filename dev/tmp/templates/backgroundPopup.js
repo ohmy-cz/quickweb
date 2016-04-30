339540f2db437783183a142cb6f0257690e612a0
ohmy-cz <cervenyjan@yahoo.co.uk>
2016-04-30 17:37:04 +0200

    Ignoring a temporary folder

--- dev/tmp/templates/backgroundPopup.js
+++
@@ -1,5 +0,0 @@
-this["AVIJST"] = this["AVIJST"] || {};
-
-this["AVIJST"]["backgroundPopup"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"avi-popup\">\r\n  <input type=\"file\" class=\"avi-boxBg\">\r\n</div>";
-},"useData":true});
